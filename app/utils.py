2d36a46f160830c8820de273e1d3b0d39ade0a9e
dev-otion-app <146097126+dev-otion-app@users.noreply.github.com>
2023-10-25 10:57:02 +0200

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -2,4 +2,7 @@ import os
 from django_config.settings import BASE_DIR
 
 def delete_images(image:str):
-    os.remove(str(BASE_DIR)+image)
+    try:
+        os.remove(str(BASE_DIR)+image)
+    except:
+        pass
